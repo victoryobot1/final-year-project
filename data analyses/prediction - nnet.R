52568d21734b63fd45e04b850d5662d1ee4ea100
victoryobot1 <victory.obot@gmail.com>
2020-03-04 15:01:10 +0000

    added better comments to the r file

--- data analyses/prediction - nnet.R
+++ data analyses/prediction - nnet.R
@@ -10,8 +10,11 @@ library(caret)
 #Read in dataset
 traffic_raw <- read.csv("joint dataset.csv", header = TRUE, sep = ",")#[,c(2,3,4,5)]
 traffic_csv <- traffic_raw[,c(1,3,7,9,11,13,15,17,18,25,26,28)]
+
+#Applying listwise deletion. (IMPUTATION)
 traffic_csv <- na.omit(traffic_csv)
 
+#Setting cattegorical variables in the dataset as factor.
 traffic_csv$ï..Description <- as.factor(traffic_csv$ï..Description)
 traffic_csv$Month <- as.factor(traffic_csv$Month)
 traffic_csv$Hour <- as.factor(traffic_csv$Hour)
@@ -21,8 +24,11 @@ traffic_csv$Temperature <- as.factor(traffic_csv$Temperature)
 traffic_csv$Weather <- as.factor(traffic_csv$Weather)
 
 table(traffic_csv$LevelofService)
- 
+
+#Set seed for reproducability in results  
 set.seed(1234)
+
+#Shuffle the data
 traffic_csv <- traffic_csv[sample(nrow(traffic_csv)),]
 
 
@@ -38,12 +44,12 @@ table(traffic_csvTrain$LevelofService)
 #create model
 losmodel <- multinom(LevelofService~., data = traffic_csvTrain,
                     maxit = 500, trace=T)
-#print(losmodel)
 
-#topModels <- varImp(losmodel)
-#topModels$Variables <- row.names(topModels)
-#topModels <- topModels[order(-topModels$Overall),]
-
+#Finding the most important variables
+mostImportantVariables <- varImp(losmodel)
+mostImportantVariables$Variables <- row.names(mostImportantVariables)
+mostImportantVariables <- mostImportantVariables[order(-mostImportantVariables$Overall),]
+print(head(mostImportantVariables))
 
 
 #holder for comparison results cross validation
