f349f806654c2db1d6ad238ba536ead0af85d185
victoryobot1 <victory.obot@gmail.com>
2020-03-04 18:20:43 +0000

    Added a package to handle listwise imputing. and changed temperature from factor type back to scale

--- data analyses/prediction - nnet.R
+++ data analyses/prediction - nnet.R
@@ -1,10 +1,10 @@
 install.packages('caret')
 install.packages('e1071')
+install.packages('TestDataImputation')
 
 library(nnet)
 library(caret)
-
-
+library(TestDataImputation)
 
 
 #Read in dataset
@@ -12,7 +12,7 @@ traffic_raw <- read.csv("joint dataset.csv", header = TRUE, sep = ",")#[,c(2,3,4
 traffic_csv <- traffic_raw[,c(1,3,7,9,11,13,15,17,18,25,26,28)]
 
 #Applying listwise deletion. (IMPUTATION)
-traffic_csv <- na.omit(traffic_csv)
+traffic_csv <- Listwise(traffic_csv, Mvalue="")
 
 #Setting cattegorical variables in the dataset as factor.
 traffic_csv$ï..Description <- as.factor(traffic_csv$ï..Description)
@@ -20,7 +20,6 @@ traffic_csv$Month <- as.factor(traffic_csv$Month)
 traffic_csv$Hour <- as.factor(traffic_csv$Hour)
 traffic_csv$DayofWeek <- as.factor(traffic_csv$DayofWeek)
 traffic_csv$TimeofDay <- as.factor(traffic_csv$TimeofDay)
-traffic_csv$Temperature <- as.factor(traffic_csv$Temperature)
 traffic_csv$Weather <- as.factor(traffic_csv$Weather)
 
 table(traffic_csv$LevelofService)
